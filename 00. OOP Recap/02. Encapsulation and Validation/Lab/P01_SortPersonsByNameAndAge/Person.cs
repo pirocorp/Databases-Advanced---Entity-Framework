f1ffadb10288125eb1aab0644829cd8d2908976a
Zdravko Zdravkov <pirocorp@abv.bg>
2019-01-03 18:25:35 +0100

    2. Salary Increase

--- 00. OOP Recap/02. Encapsulation and Validation/Lab/P01_SortPersonsByNameAndAge/Person.cs
+++ 00. OOP Recap/02. Encapsulation and Validation/Lab/P01_SortPersonsByNameAndAge/Person.cs
@@ -9,24 +9,39 @@
         private readonly string firstName;
         private readonly string lastName;
         private readonly int age;
+        private decimal salary;
 
-        public Person(string firstName, string lastName, int age)
+        public Person(string firstName, string lastName, int age, decimal salary)
         {
             this.firstName = firstName;
             this.lastName = lastName;
             this.age = age;
+            this.salary = salary;
         }
-
-
+        
         public string FirstName => this.firstName;
 
         public string LastName => this.lastName;
 
         public int Age => this.age;
 
+        public decimal Salary => this.salary;
+
+        public void IncreaseSalary(decimal percent)
+        {
+            if (this.Age > 30)
+            {
+                this.salary += this.salary * percent / 100;
+            }
+            else
+            {
+                this.salary += this.salary * percent / 200;
+            }
+        }
+
         public override string ToString()
         {
-            return $"{this.FirstName} {this.LastName} is a {this.Age} years old.";
+            return $"{this.FirstName} {this.LastName} get {this.Salary:F2} leva.";
         }
     }
 }
--- 00. OOP Recap/02. Encapsulation and Validation/Lab/P01_SortPersonsByNameAndAge/StartUp.cs
+++ 00. OOP Recap/02. Encapsulation and Validation/Lab/P01_SortPersonsByNameAndAge/StartUp.cs
@@ -8,20 +8,39 @@
     {
         public static void Main()
         {
+            //Problem 1
+            //var lines = int.Parse(Console.ReadLine());
+            //var persons = new List<Person>();
+            //for (int i = 0; i < lines; i++)
+            //{
+            //    var cmdArgs = Console.ReadLine().Split();
+            //    var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+            //    persons.Add(person);
+            //}
+            //
+            //persons.OrderBy(p => p.FirstName)
+            //    .ThenBy(p => p.Age)
+            //    .ToList()
+            //    .ForEach(p => Console.WriteLine(p.ToString()));
+
+            //Problem 2
             var lines = int.Parse(Console.ReadLine());
             var persons = new List<Person>();
+
             for (int i = 0; i < lines; i++)
             {
                 var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                var person = new Person(cmdArgs[0],
+                    cmdArgs[1],
+                    int.Parse(cmdArgs[2]),
+                    decimal.Parse(cmdArgs[3]));
+
                 persons.Add(person);
             }
 
-            persons.OrderBy(p => p.FirstName)
-                .ThenBy(p => p.Age)
-                .ToList()
-                .ForEach(p => Console.WriteLine(p.ToString()));
+            var bonus = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(bonus));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
         }
-
     }
 }
