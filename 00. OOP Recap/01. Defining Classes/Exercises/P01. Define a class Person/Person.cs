5bc254ef8064b810e41e53b0b77e93bcc77b53b0
Zdravko Zdravkov <pirocorp@abv.bg>
2019-01-03 16:29:51 +0100

    P01. Define a class Person

---
+++ 00. OOP Recap/01. Defining Classes/Exercises/P01. Define a class Person/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace P01._Define_a_class_Person
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }    
+    }
+}
---
+++ 00. OOP Recap/01. Defining Classes/Exercises/P01. Define a class Person/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace P01._Define_a_class_Person
+{
+    using System;
+    using System.Reflection;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var personType = typeof(Person);
+            var properties = personType.GetProperties
+                (BindingFlags.Public | BindingFlags.Instance);
+
+            Console.WriteLine(properties.Length);
+
+            var person1 = new Person { Name = "Pesho", Age = 20};
+            var person2 = new Person { Name = "Gosho", Age = 18 };
+            var person3 = new Person();
+            person3.Name = "Stamat";
+            person3.Age = 43;
+        }
+    }
+}
