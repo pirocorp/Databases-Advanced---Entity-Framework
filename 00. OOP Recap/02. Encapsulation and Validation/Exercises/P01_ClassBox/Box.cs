5510f0d418520856b3657fb5e0953b6dd5ff4ca3
Zdravko Zdravkov <pirocorp@abv.bg>
2019-01-03 19:07:31 +0100

    P01_ClassBox

---
+++ 00. OOP Recap/02. Encapsulation and Validation/Exercises/P01_ClassBox/Box.cs
@@ -0,0 +1,38 @@
+﻿namespace P01_ClassBox
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box
+    {
+        private decimal x;
+        private decimal y;
+        private decimal z;
+
+        public Box(decimal x, decimal y, decimal z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public decimal Volume()
+        {
+            var result = this.x * this.y * this.z;
+            return result;
+        }
+        
+        public decimal LateralSurfaceArea()
+        {
+            var result = 2 * this.x * this.y + 2 * this.z * this.y;
+            return result;
+        }
+
+        public decimal SurfaceArea()
+        {
+            var result = this.LateralSurfaceArea() + 2 * this.x * this.z;
+            return result;
+        }
+    }
+}
---
+++ 00. OOP Recap/02. Encapsulation and Validation/Exercises/P01_ClassBox/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace P01_ClassBox
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var boxType = typeof(Box);
+            var fields = boxType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine(fields.Count());
+
+            var x = decimal.Parse(Console.ReadLine());
+            var z = decimal.Parse(Console.ReadLine());
+            var y = decimal.Parse(Console.ReadLine());
+
+            var box = new Box(x, y, z);
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+            Console.WriteLine($"Volume - {box.Volume():F2}");
+        }
+    }
+}
