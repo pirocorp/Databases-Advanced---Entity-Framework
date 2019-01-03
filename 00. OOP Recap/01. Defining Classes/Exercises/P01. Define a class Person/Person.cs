0c91678c7e08c712e0c1b72a021f96329717343d
Zdravko Zdravkov <pirocorp@abv.bg>
2019-01-03 16:36:45 +0100

    Problem 2. Creating Constructors

--- 00. OOP Recap/01. Defining Classes/Exercises/P01. Define a class Person/Person.cs
+++ 00. OOP Recap/01. Defining Classes/Exercises/P01. Define a class Person/Person.cs
@@ -2,6 +2,24 @@
 {
     public class Person
     {
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age)
+            :this()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+            :this(age)
+        {
+            this.Name = name;
+        }
+
         public string Name { get; set; }
 
         public int Age { get; set; }    
--- 00. OOP Recap/01. Defining Classes/Exercises/P01. Define a class Person/StartUp.cs
+++ 00. OOP Recap/01. Defining Classes/Exercises/P01. Define a class Person/StartUp.cs
@@ -7,17 +7,41 @@
     {
         public static void Main()
         {
+            //Problem 1
+            //var personType = typeof(Person);
+            //var properties = personType.GetProperties
+            //    (BindingFlags.Public | BindingFlags.Instance);
+
+            //Console.WriteLine(properties.Length);
+
+            //var person1 = new Person { Name = "Pesho", Age = 20};
+            //var person2 = new Person { Name = "Gosho", Age = 18 };
+            //var person3 = new Person();
+            //person3.Name = "Stamat";
+            //person3.Age = 43;
+
+            //Problem 2
             var personType = typeof(Person);
-            var properties = personType.GetProperties
-                (BindingFlags.Public | BindingFlags.Instance);
+            var emptyCtor = personType.GetConstructor(new Type[] { });
+            var ageCtor = personType.GetConstructor(new[] { typeof(int) });
+            var nameAgeCtor = personType.GetConstructor(new[] { typeof(string), typeof(int) });
+            var swapped = false;
+            if (nameAgeCtor == null)
+            {
+                nameAgeCtor = personType.GetConstructor(new[] { typeof(int), typeof(string) });
+                swapped = true;
+            }
 
-            Console.WriteLine(properties.Length);
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
 
-            var person1 = new Person { Name = "Pesho", Age = 20};
-            var person2 = new Person { Name = "Gosho", Age = 18 };
-            var person3 = new Person();
-            person3.Name = "Stamat";
-            person3.Age = 43;
+            var basePerson = (Person)emptyCtor.Invoke(new object[] { });
+            var personWithAge = (Person)ageCtor.Invoke(new object[] { age });
+            var personWithAgeAndName = swapped ? (Person)nameAgeCtor.Invoke(new object[] { age, name }) : (Person)nameAgeCtor.Invoke(new object[] { name, age });
+
+            Console.WriteLine("{0} {1}", basePerson.Name, basePerson.Age);
+            Console.WriteLine("{0} {1}", personWithAge.Name, personWithAge.Age);
+            Console.WriteLine("{0} {1}", personWithAgeAndName.Name, personWithAgeAndName.Age);
         }
     }
 }
