6c2655a5a951748f71e2a9de3840c89b39a44f42
Zdravko Zdravkov <pirocorp@abv.bg>
2019-01-03 18:16:09 +0100

    P01_SortPersonsByNameAndAge

---
+++ 00. OOP Recap/02. Encapsulation and Validation/Lab/P01_SortPersonsByNameAndAge/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace P01_SortPersonsByNameAndAge
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Person
+    {
+        private readonly string firstName;
+        private readonly string lastName;
+        private readonly int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+
+        public string FirstName => this.firstName;
+
+        public string LastName => this.lastName;
+
+        public int Age => this.age;
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is a {this.Age} years old.";
+        }
+    }
+}
