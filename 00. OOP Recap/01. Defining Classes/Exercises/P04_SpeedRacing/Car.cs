91abc50867d5ed7d6d550cab26ee0bf2963ee449
Zdravko Zdravkov <pirocorp@abv.bg>
2019-01-03 18:06:37 +0100

    P04_SpeedRacing

---
+++ 00. OOP Recap/01. Defining Classes/Exercises/P04_SpeedRacing/Car.cs
@@ -0,0 +1,43 @@
+﻿namespace P04_SpeedRacing
+{
+    using System;
+
+    public class Car
+    {
+        public Car(string model, decimal fuelAmount, decimal fuelConsumptionPerKm)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKm = fuelConsumptionPerKm;
+            this.DistanceTraveled = 0;
+        }
+
+        public string Model { get; set; }
+
+        public decimal FuelAmount { get; set; }
+
+        public decimal FuelConsumptionPerKm { get; set; }
+
+        public decimal DistanceTraveled { get; set; }
+
+        public void Drive(decimal distance)
+        {
+            var reachableDistance = this.FuelAmount / this.FuelConsumptionPerKm;
+
+            if (reachableDistance >= distance)
+            {
+                this.FuelAmount -= this.FuelConsumptionPerKm * distance;
+                this.DistanceTraveled += distance;
+                return;
+            }
+
+            throw new ArgumentException("Insufficient fuel for the drive");
+        }
+
+        public override string ToString()
+        {
+            var result = $"{this.Model} {this.FuelAmount:F2} {this.DistanceTraveled}";
+            return result;
+        }
+    }
+}
---
+++ 00. OOP Recap/01. Defining Classes/Exercises/P04_SpeedRacing/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace P04_SpeedRacing
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var cars = new Dictionary<string, Car>();
+            var n = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < n; i++)
+            {
+                var inputArgs = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                var model = inputArgs[0];
+                var fuelAmount = decimal.Parse(inputArgs[1]);
+                var consumption = decimal.Parse(inputArgs[2]);
+
+                var currentCar = new Car(model, fuelAmount, consumption);
+                cars.Add(model, currentCar);
+            }
+
+            var input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                var inputArgs = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                ;
+                var model = inputArgs[1];
+                var distance = decimal.Parse(inputArgs[2]);
+
+                try
+                {
+                    cars[model].Drive(distance);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+
+            foreach (var car in cars.Values)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
