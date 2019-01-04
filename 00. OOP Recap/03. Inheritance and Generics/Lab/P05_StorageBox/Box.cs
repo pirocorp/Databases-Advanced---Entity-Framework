5388b0c22c98c14ee9336ef8d01b3a849cb14fc0
Zdravko Zdravkov <pirocorp@abv.bg>
2019-01-04 11:02:00 +0100

    P05_StorageBox

---
+++ 00. OOP Recap/03. Inheritance and Generics/Lab/P05_StorageBox/Box.cs
@@ -0,0 +1,48 @@
+﻿namespace P05_StorageBox
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private T[] data;
+        private int count;
+
+        public Box()
+        {
+            this.data = new T[4];
+            this.count = 0;
+        }
+
+        public int Count => this.count;
+
+        public void Add(T item)
+        {
+            if (this.count == this.data.Length)
+            {
+                var oldData = this.data;
+                this.data = new T[this.count * 2];
+                oldData.CopyTo(this.data, 0);
+            }
+
+            this.data[this.count] = item;
+            this.count++;
+        }
+
+        public T Remove()
+        {
+            var lastIndex = this.count - 1;
+            var lastElement = this.data[lastIndex];
+            this.data[lastIndex] = default(T);
+            this.count--;
+            return lastElement;
+        }
+
+        public override string ToString()
+        {
+            var result = string.Join(", ", this.data);
+            return result;
+        }
+    }
+}
---
+++ 00. OOP Recap/03. Inheritance and Generics/Lab/P05_StorageBox/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace P05_StorageBox
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var box = new Box<string>();
+
+            for (var i = 0; i < 20; i++)
+            {
+                box.Add($"Item {i}");
+            }
+
+            while (box.Count > 0)
+            {
+                Console.WriteLine(box.Remove());
+            }
+        }
+    }
+}
