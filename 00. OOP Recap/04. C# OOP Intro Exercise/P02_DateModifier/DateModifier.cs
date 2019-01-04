dba0eb48757afb9816892f50823adbc817b59ed7
Zdravko Zdravkov <pirocorp@abv.bg>
2019-01-04 14:54:01 +0100

    P02_DateModifier

---
+++ 00. OOP Recap/04. C# OOP Intro Exercise/P02_DateModifier/DateModifier.cs
@@ -0,0 +1,34 @@
+﻿namespace P02_DateModifier
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class DateModifier
+    {
+        private DateTime firstDate;
+        private DateTime secondDate;
+
+        public DateModifier()
+        {
+        }
+
+        public int DayDifference(string firstDateString, string secondDateString)
+        {
+            this.firstDate = this.ParseDate(firstDateString);
+            this.secondDate = this.ParseDate(secondDateString);
+            var result = (int)this.firstDate.Subtract(this.secondDate).TotalDays;
+            return Math.Abs(result);
+        }
+
+        private DateTime ParseDate(string firstDateString)
+        {
+            var firstDateTokens = firstDateString.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+            var year = int.Parse(firstDateTokens[0]);
+            var month = int.Parse(firstDateTokens[1]);
+            var day = int.Parse(firstDateTokens[2]);
+            var result = new DateTime(year, month, day);
+            return result;
+        }
+    }
+}
---
+++ 00. OOP Recap/04. C# OOP Intro Exercise/P02_DateModifier/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace P02_DateModifier
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstDate = Console.ReadLine();
+            var secondDate = Console.ReadLine();
+
+            var dateModifier = new DateModifier();
+            Console.WriteLine(dateModifier.DayDifference(firstDate, secondDate));
+        }
+    }
+}
