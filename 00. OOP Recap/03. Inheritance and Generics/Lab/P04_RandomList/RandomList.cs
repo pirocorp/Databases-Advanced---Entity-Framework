5d441e234f9d25980b261229ebc0db8339688739
Zdravko Zdravkov <pirocorp@abv.bg>
2019-01-04 10:44:53 +0100

    P04_RandomList

---
+++ 00. OOP Recap/03. Inheritance and Generics/Lab/P04_RandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿namespace P04_RandomList
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class RandomList : List<string>
+    {
+        private readonly Random rnd;
+
+        public RandomList()
+            :base()
+        {
+            this.rnd = new Random();
+        }
+
+        public string RandomString()
+        {
+            var index = this.rnd.Next(0, this.Count - 1);
+            var result = this[index];
+            this.RemoveAt(index);
+            return result;
+        }
+    }
+}
---
+++ 00. OOP Recap/03. Inheritance and Generics/Lab/P04_RandomList/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace P04_RandomList
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var randomList = new RandomList();
+
+            for (var i = 0; i < 10; i++)
+            {
+                randomList.Add($"Item {i}");
+            }
+
+            var randomElement = randomList.RandomString();
+            Console.WriteLine(randomElement);
+            Console.WriteLine(string.Join(", ", randomList));
+        }
+    }
+}
