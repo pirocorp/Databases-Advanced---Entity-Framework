9e25bc35b68ed0b1e66ef5d34e5b30f5befcd33d
Zdravko Zdravkov <pirocorp@abv.bg>
2019-01-09 16:27:57 +0100

    P09_IncreaseAgeStoredProcedure

---
+++ 01. DB Apps Introduction/Exercises/P09_IncreaseAgeStoredProcedure/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P09_IncreaseAgeStoredProcedure
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=PIROMAN\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
+    }
+}
---
+++ 01. DB Apps Introduction/Exercises/P09_IncreaseAgeStoredProcedure/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace P09_IncreaseAgeStoredProcedure
+{
+    using System;
+    using System.Data;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var minionId = int.Parse(Console.ReadLine());
+            using (var dbCon = new SqlConnection(Configuration.ConnectionString))
+            {
+                dbCon.Open();
+                var usp = "usp_GetOlder";
+                using (var uspCommand = new SqlCommand(usp, dbCon))
+                {
+                    uspCommand.CommandType = CommandType.StoredProcedure;
+                    uspCommand.Parameters.AddWithValue("@minionId", minionId);
+                    uspCommand.ExecuteNonQuery();
+                }
+
+                var getMinionData = "SELECT Name, Age FROM Minions WHERE Id = @minionId";
+                using (var getMinionDataCommand = new SqlCommand(getMinionData, dbCon))
+                {
+                    getMinionDataCommand.Parameters.AddWithValue("@minionId", minionId);
+                    var dataReader = getMinionDataCommand.ExecuteReader();
+                    using (dataReader)
+                    {
+                        while (dataReader.Read())
+                        {
+                            Console.WriteLine($"{dataReader[0]} – {dataReader[1]} years old");
+                        }
+                    }
+                }
+
+                dbCon.Close();
+            }
+        }
+    }
+}
