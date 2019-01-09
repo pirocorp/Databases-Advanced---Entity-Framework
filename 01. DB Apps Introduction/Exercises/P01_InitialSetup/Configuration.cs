b7df8a888936be06a9edd6877492f3bee414cb26
Zdravko Zdravkov <pirocorp@abv.bg>
2019-01-09 10:56:52 +0100

    P01_InitialSetup

---
+++ 01. DB Apps Introduction/Exercises/P01_InitialSetup/Configuration.cs
@@ -0,0 +1,11 @@
+﻿namespace P01_InitialSetup
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=PIROMAN\SQLEXPRESS;Integrated Security=true";
+    }
+}
