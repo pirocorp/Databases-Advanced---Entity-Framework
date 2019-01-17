6c1cac863d9c6820172d4eb5995567c7b0e7ff8a
Zdravko Zdravkov <pirocorp@abv.bg>
2019-01-17 16:59:25 +0100

    03A. Entity-Framework-Performance

---
+++ 03A. Entity-Framework-Performance/Demo/Solving-ToList-Problem/SolvingToListProblem.cs
@@ -0,0 +1,91 @@
+﻿namespace Solving_ToList_Problem
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SolvingToListProblem
+    {
+        public static void Main()
+        {
+            //IncorrectSelectExample();
+
+            //CorrectSelectExample();
+
+            //ShowCustomersPurchasedFromTokyo_Slow();
+
+            //ShowCustomersPurchasedFromTokyo_Fast();
+        }
+
+        private static void CorrectSelectExample()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                context.Products
+                    .Where(p => p.CategoryID == 3)
+                    .Select(e => new
+                    {
+                        e.ProductName
+                    })
+                    .ToList()
+                    .ForEach(i =>
+                    {
+                        Console.WriteLine(i.ProductName);
+                    });
+            }
+        }
+
+        private static void IncorrectSelectExample()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                context.Products
+                    .Where(p => p.CategoryID == 3)
+                    .ToList()
+                    .ForEach(i =>
+                    {
+                        Console.WriteLine(i.ProductName);
+                    });
+            }
+        }
+
+        private static void ShowCustomersPurchasedFromTokyo_Slow()
+        {
+            List<Order_Detail> orderItemsFromTokyo;
+
+            using (var context = new NorthwindEntities())
+            {
+                orderItemsFromTokyo = context.Order_Details.ToList()
+                    .Where(od => od.Product.Supplier.City == "Tokyo")
+                    .ToList();
+            }
+
+            var customerPurchasedFromTokyo = orderItemsFromTokyo
+                .Select(oi => oi.Order.Customer)
+                .ToList();
+
+            foreach (var customer in customerPurchasedFromTokyo)
+            {
+                Console.WriteLine(customer.ContactName);
+            }
+        }
+
+        private static void ShowCustomersPurchasedFromTokyo_Fast()
+        {
+            List<Customer> customerPurchasedFromTokyo;
+
+            using (var context = new NorthwindEntities())
+            {
+                customerPurchasedFromTokyo = context.Order_Details
+                    .Where(orderItem => orderItem.Product.Supplier.City == "Tokyo")
+                    .Select(orderItem => orderItem.Order.Customer)
+                    .ToList();
+            }
+
+            foreach (var customer in customerPurchasedFromTokyo)
+            {
+                Console.WriteLine(customer.ContactName);
+            }
+        }
+    }
+}
---
+++ 03A. Entity-Framework-Performance/Demo/SolvingTheNplus1QueryProblem/SolvingTheNplus1QueryProblem.cs
@@ -0,0 +1,60 @@
+﻿namespace ResolvingTheNplus1QueryProblem
+{
+    using System;
+    using System.Linq;
+
+    public class SolvingTheNplus1QueryProblem
+    {
+        public static void Main()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                //BestWay(context);
+
+                //PrintCustomersAndRegionsWithQueryProblem(context);
+
+                //PrintCustomersAndRegionsWithoutQueryProblem(context);
+            }
+        }
+
+        private static void BestWay(NorthwindEntities context)
+        {
+            var products = context.Products
+                .Select(p => new
+                {
+                    p.ProductName,
+                    p.Supplier.CompanyName,
+                    p.Category.CategoryName
+                })
+                .ToList();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product: {product.ProductName}; Supplier: {product.CompanyName}; Category: {product.CategoryName}");
+            }
+        }
+
+        private static void PrintCustomersAndRegionsWithQueryProblem(NorthwindEntities context)
+        {
+            var products = context.Products
+                .ToList();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product: {product.ProductName}; Supplier: {product.Supplier.CompanyName}; Category: {product.Category.CategoryName}");
+            }
+        }
+
+        private static void PrintCustomersAndRegionsWithoutQueryProblem(NorthwindEntities context)
+        {
+            var products = context.Products
+                .Include("Supplier")
+                .Include("Category");
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product: {product.ProductName}; Supplier: {product.Supplier.CompanyName}; Category: {product.Category.CategoryName}");
+            }
+        }
+    }
+}
