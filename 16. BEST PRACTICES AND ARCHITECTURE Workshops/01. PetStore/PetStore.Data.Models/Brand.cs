9c0ac8133805c2507c7160afa92a5fda7b50636f
Zdravko Zdravkov <zdr0@yahoo.com>
2020-04-28 17:19:55 +0200

    01. PetStore Ready for verification

--- 16. BEST PRACTICES AND ARCHITECTURE Workshops/01. PetStore/PetStore.Data.Models/Brand.cs
+++ 16. BEST PRACTICES AND ARCHITECTURE Workshops/01. PetStore/PetStore.Data.Models/Brand.cs
@@ -1,11 +1,13 @@
 ﻿namespace PetStore.Data.Models
 {
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
     public class Brand
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public ICollection<Toy> Toys { get; set; } = new HashSet<Toy>();
