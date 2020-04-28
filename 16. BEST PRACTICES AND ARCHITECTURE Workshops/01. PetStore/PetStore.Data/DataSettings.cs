5069777d61641f1f8ddde73d1801a774dddc4bbc
Zdravko Zdravkov <zdr0@yahoo.com>
2020-04-28 18:32:36 +0200

    Move project to folders

--- 16. BEST PRACTICES AND ARCHITECTURE Workshops/01. PetStore/PetStore.Data/DataSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PetStore.Data
-{
-    public static class DataSettings
-    {
-        public const string Connection = "Server=PIRO\\SQLEXPRESS2019;Database=PetStoreCatalog;Integrated Security=True;";
-    }
-}
--- 16. BEST PRACTICES AND ARCHITECTURE Workshops/01. PetStore/PetStore/StartUp.cs
+++ 16. BEST PRACTICES AND ARCHITECTURE Workshops/01. PetStore/PetStore/StartUp.cs
@@ -6,7 +6,7 @@
     {
         public static void Main()
         {
-            
+            Console.WriteLine("Hello World!");
         }
     }
 }
