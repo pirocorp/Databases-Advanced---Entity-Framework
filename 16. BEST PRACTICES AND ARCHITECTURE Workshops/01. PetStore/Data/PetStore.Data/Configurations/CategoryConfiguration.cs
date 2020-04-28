c01ff03a211d8124343d0c53d509159bcc12a97f
Zdravko Zdravkov <zdr0@yahoo.com>
2020-04-28 18:33:15 +0200

    Moving projects to folders

---
+++ 16. BEST PRACTICES AND ARCHITECTURE Workshops/01. PetStore/Data/PetStore.Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,31 @@
+﻿namespace PetStore.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Models;
+
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> category)
+        {
+            //Many to One
+            category
+                .HasMany(c => c.Pets)
+                .WithOne(p => p.Category)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            category
+                .HasMany(c => c.Foods)
+                .WithOne(f => f.Category)
+                .HasForeignKey(f => f.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            category
+                .HasMany(c => c.Toys)
+                .WithOne(t => t.Category)
+                .HasForeignKey(t => t.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
