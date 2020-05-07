cd7efffcd840fab9234a31a3ee7ddb5073106451
Zdravko Zdravkov <pirocorp@abv.bg>
2020-05-07 16:23:59 +0200

    01. Model Defition && 02. Data Import

---
+++ 14. Exams/C# DB Advanced Exam Retake - 18 Apr 2019/MusicHub/DataProcessor/ImportDtos/PerformerImportDto.cs
@@ -0,0 +1,32 @@
+﻿namespace MusicHub.DataProcessor.ImportDtos
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Serialization;
+
+    [XmlType("Performer")]
+    public class PerformerImportDto
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        [XmlElement("FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        [XmlElement("LastName")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(18, 70)]
+        [XmlElement("Age")]
+        public int Age { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        [XmlElement("NetWorth")]
+        public decimal NetWorth { get; set; }
+
+        [XmlArray("PerformersSongs")]
+        public PerformerSongImportDto[] PerformersSongs { get; set; }
+    }
+}
---
+++ 14. Exams/C# DB Advanced Exam Retake - 18 Apr 2019/MusicHub/DataProcessor/ImportDtos/ProducerImportDto.cs
@@ -0,0 +1,25 @@
+﻿namespace MusicHub.DataProcessor.ImportDtos
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ProducerImportDto
+    {
+        public ProducerImportDto()
+        {
+            this.Albums = new List<ProducerAlbumImportDto>();
+        }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [RegularExpression("[A-Z][a-z]+ [A-Z][a-z]+")]
+        public string Pseudonym { get; set; }
+
+        [RegularExpression("\\+359 [0-9]{3} [0-9]{3} [0-9]{3}")]
+        public string PhoneNumber { get; set; }
+
+        public List<ProducerAlbumImportDto> Albums { get; set; }
+    }
+}
