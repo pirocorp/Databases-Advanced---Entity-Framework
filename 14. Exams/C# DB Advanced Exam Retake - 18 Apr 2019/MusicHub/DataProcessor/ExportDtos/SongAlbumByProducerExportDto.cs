e39a8ed7d688ddf95eaf874afcf66e7815dfac0c
Zdravko Zdravkov <pirocorp@abv.bg>
2020-05-07 17:24:19 +0200

    03. Data Export

---
+++ 14. Exams/C# DB Advanced Exam Retake - 18 Apr 2019/MusicHub/DataProcessor/ExportDtos/SongAlbumByProducerExportDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MusicHub.DataProcessor.ExportDtos
+{
+    public class SongAlbumByProducerExportDto
+    {
+        public string SongName { get; set; }
+
+        public string Price { get; set; }
+
+        public string Writer { get; set; }
+    }
+}
