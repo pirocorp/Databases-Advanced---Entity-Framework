73d91eca2e8197d51c537285f179c0fe27186022
Zdravko Zdravkov <zdr0@yahoo.com>
2021-12-02 11:43:05 +0100

    06. Extensible Markup Language - XML

--- Exercises/06. Extensible Markup Language - XML/CarDealer/Data/CarDealerContext.cs
+++ Exercises/06. Extensible Markup Language - XML/CarDealer/Data/CarDealerContext.cs
@@ -16,10 +16,15 @@
         }
 
         public DbSet<Car> Cars { get; set; }
+
         public DbSet<Customer> Customers { get; set; }
+
         public DbSet<Part> Parts { get; set; }
+
         public DbSet<PartCar> PartCars { get; set; }
+
         public DbSet<Sale> Sales { get; set; }
+
         public DbSet<Supplier> Suppliers { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
